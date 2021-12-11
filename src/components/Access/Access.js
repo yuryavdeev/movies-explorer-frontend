cd62c2db1b555afca6f48e7da90a04b5d60b3bf5
Yury Avdeev <avdeiev@gmail.com>
2021-12-11 22:43:48 +0300

    update

--- src/components/Access/Access.js
+++ src/components/Access/Access.js
@@ -6,124 +6,124 @@ import logo from '../../images/logo.svg'
 
 const Access = React.memo(({ nextHandleSubmit, greeting, button, isRegistrated, link, messageErr }) => {
 
-    const location = useLocation()
-    const history = useHistory()
-    const handleForm = useFormWithValidation()
-    const [buttonDisabled, setButtonDisabled] = React.useState(true)
+  const location = useLocation()
+  const history = useHistory()
+  const handleForm = useFormWithValidation()
+  const [buttonDisabled, setButtonDisabled] = React.useState(true)
 
 
-    React.useEffect(() => {
-        handleForm.isValid ? setButtonDisabled(false) : setButtonDisabled(true)
-    }, [handleForm.isValid])
+  React.useEffect(() => {
+    handleForm.isValid ? setButtonDisabled(false) : setButtonDisabled(true)
+  }, [handleForm.isValid])
 
 
-    const handleSubmit = (evt) => {
-        evt.preventDefault()
-        nextHandleSubmit(handleForm.values)
-        handleForm.resetForm()
-    }
+  const handleSubmit = (evt) => {
+    evt.preventDefault()
+    nextHandleSubmit(handleForm.values)
+    handleForm.resetForm()
+  }
 
 
-    return (
-        <div className='access'>
+  return (
+    <div className='access'>
 
-            <div className='access__top'>
-                <img className='logo' src={logo} onClick={() => history.push('/')} alt='логотип' />
-                <h2 className='access__title'>{greeting}</h2>
-            </div>
+      <div className='access__top'>
+        <img className='logo' src={logo} onClick={() => history.push('/')} alt='логотип' />
+        <h2 className='access__title'>{greeting}</h2>
+      </div>
 
-            <form className='access__form' onSubmit={handleSubmit} id='access'>
-                {location.pathname === '/signup' &&
-                    <>
-                        <label className='access__label'>Имя
-                            <input
-                                id='name'
-                                name='name'
-                                type='text'
-                                placeholder='name'
-                                className={`access__input ${handleForm.errors.name && 'access__input_wrong'}`}
-                                value={handleForm.values.this}
-                                onChange={handleForm.handleChange}
-                                // formNoValidate
-                                minLength='2'
-                                maxLength='30'
-                                pattern='^[A-Za-zА-Яа-яЁё\s\-]{2,30}$'
-                                title="поле может содержать только латиницу, кириллицу, пробел или дефис"
-                            />
-                        </label>
-                        {
-                            handleForm.errors.name &&
-                            <span className='access__input-error'>{handleForm.errors.name}</span>
-                        }
-                    </>
-                }
+      <form className='access__form' onSubmit={handleSubmit} id='access'>
+        {location.pathname === '/signup' &&
+          <>
+            <label className='access__label'>Имя
+              <input
+                id='name'
+                name='name'
+                type='text'
+                placeholder='name'
+                className={`access__input ${handleForm.errors.name && 'access__input_wrong'}`}
+                value={handleForm.values.this}
+                onChange={handleForm.handleChange}
+                // formNoValidate
+                minLength='2'
+                maxLength='30'
+                pattern='^[A-Za-zА-Яа-яЁё\s\-]{2,30}$'
+                title="поле может содержать только латиницу, кириллицу, пробел или дефис"
+              />
+            </label>
+            {
+              handleForm.errors.name &&
+              <span className='access__input-error'>{handleForm.errors.name}</span>
+            }
+          </>
+        }
 
-                <label className='access__label'>E-mail
-                    <input
-                        id='email'
-                        name='email'
-                        type='email'
-                        placeholder='e-mail'
-                        className={`access__input ${handleForm.errors.email && 'access__input_wrong'}`}
-                        value={handleForm.values.this}
-                        onChange={handleForm.handleChange}
-                        // formNoValidate
-                        pattern='^[^@\s]+@[^@\s]+\.[^@\s]+$'
-                        title="поле должно содержать формат электронного адреса"
-                    />
-                </label>
-                {
-                    handleForm.errors.email &&
-                    <span className='access__input-error'>{handleForm.errors.email}</span>
-                }
+        <label className='access__label'>E-mail
+          <input
+            id='email'
+            name='email'
+            type='email'
+            placeholder='e-mail'
+            className={`access__input ${handleForm.errors.email && 'access__input_wrong'}`}
+            value={handleForm.values.this}
+            onChange={handleForm.handleChange}
+            // formNoValidate
+            pattern='^[^@\s]+@[^@\s]+\.[^@\s]+$'
+            title="поле должно содержать формат электронного адреса"
+          />
+        </label>
+        {
+          handleForm.errors.email &&
+          <span className='access__input-error'>{handleForm.errors.email}</span>
+        }
 
-                <label className='access__label'>Пароль
-                    <input
-                        id='password'
-                        name='password'
-                        type='password'
-                        placeholder='password'
-                        className={`access__input ${handleForm.errors.password && 'access__input_wrong'}`}
-                        value={handleForm.values.this}
-                        onChange={handleForm.handleChange}
-                        // formNoValidate
-                        minLength='6'
-                        maxLength='30'
-                        title="поле должно содержать не менее 6 и не более 30 знаков"
-                    />
-                </label>
-                {
-                    handleForm.errors.password &&
-                    <span className='access__input-error'>{handleForm.errors.password}</span>
-                }
+        <label className='access__label'>Пароль
+          <input
+            id='password'
+            name='password'
+            type='password'
+            placeholder='password'
+            className={`access__input ${handleForm.errors.password && 'access__input_wrong'}`}
+            value={handleForm.values.this}
+            onChange={handleForm.handleChange}
+            // formNoValidate
+            minLength='6'
+            maxLength='30'
+            title="поле должно содержать не менее 6 и не более 30 знаков"
+          />
+        </label>
+        {
+          handleForm.errors.password &&
+          <span className='access__input-error'>{handleForm.errors.password}</span>
+        }
 
-            </form>
+      </form>
 
-            <div className='access__bottom'>
-                {messageErr &&
-                    <p className="access__message">{messageErr}</p>}
+      <div className='access__bottom'>
+        {messageErr &&
+          <p className="access__message">{messageErr}</p>}
 
-                <button
-                    className={`access__submit ${buttonDisabled && 'access__submit_disabled'}`}
-                    form='access'
-                    disabled={buttonDisabled}
-                    type='submit'
-                >
-                    {button}
-                </button>
+        <button
+          className={`access__submit ${buttonDisabled && 'access__submit_disabled'}`}
+          form='access'
+          disabled={buttonDisabled}
+          type='submit'
+        >
+          {button}
+        </button>
 
-                <p className='access__is-registrated'>{isRegistrated}
-                    <Link
-                        to={location.pathname === '/signin' ? '/signup' : '/signin'}
-                        className='access__link'
-                        target='_self'
-                    >
-                        {link}
-                    </Link>
-                </p>
-            </div>
-        </div>
-    )
+        <p className='access__is-registrated'>{isRegistrated}
+          <Link
+            to={location.pathname === '/signin' ? '/signup' : '/signin'}
+            className='access__link'
+            target='_self'
+          >
+            {link}
+          </Link>
+        </p>
+      </div>
+    </div>
+  )
 })
 
 export default Access
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,68 +10,68 @@ import { CurrentUser } from '../../contexts/CurrentUserContext'
 
 const Header = React.memo(() => {
 
-    const currentUser = React.useContext(CurrentUser)
-    const location = useLocation() // или > { pathname } = useLocation()
-    const history = useHistory()
-    const [navigationPopup, setNavigationPopup] = React.useState(false)
-    const resolution = window.matchMedia('(min-width: 800px)') // медиа-запрос в конст., возвр. объект
-    const [viewportWidth, setViewportWidth] = React.useState(resolution.matches) // св-во объекта ответа - true - если document сейчас соот-т медиа-запросу
+  const currentUser = React.useContext(CurrentUser)
+  const location = useLocation() // или > { pathname } = useLocation()
+  const history = useHistory()
+  const [navigationPopup, setNavigationPopup] = React.useState(false)
+  const resolution = window.matchMedia('(min-width: 800px)') // медиа-запрос в конст., возвр. объект
+  const [viewportWidth, setViewportWidth] = React.useState(resolution.matches) // св-во объекта ответа - true - если document сейчас соот-т медиа-запросу
 
 
-    React.useEffect(() => {
-        resolution.onchange = () => { // переход через 800рх
-            resolution.matches ? // => true / false
-                setViewportWidth(true)
+  React.useEffect(() => {
+    resolution.onchange = () => { // переход через 800рх
+      resolution.matches ? // => true / false
+        setViewportWidth(true)
+        :
+        setViewportWidth(false)
+    }
+    return resolution.onchange
+  }, [resolution])
+
+
+  const openPopup = () => {
+    setNavigationPopup(true)
+  }
+
+
+  const closePopup = () => {
+    setNavigationPopup(false)
+  }
+
+
+  return (
+    <header className={`header ${location.pathname === '/' && 'header__type_color'}`}>
+
+      <div className='header__wrap'>
+        <img className='logo' src={logo} onClick={() => history.push('/')} alt='логотип' />
+        {
+          !currentUser ?
+            <nav className='header__nav-container'>
+              <Link to='/signup' className='header__link_word' target='_self'>Регистрация</Link>
+              <Link to='/signin' className='header__link_button' target='_self'>Войти</Link>
+            </nav>
+            :
+            <>
+              {viewportWidth ?
+                <Navigation />
                 :
-                setViewportWidth(false)
+                <img className='header__nav-icon' src={menuImage} alt='иконка меню' onClick={openPopup} />
+              }
+            </>
         }
-        return resolution.onchange
-    }, [resolution])
+        {
+          navigationPopup &&
+          <Popup
+            closePopup={closePopup}
+            Content={<Navigation />}
+            buttonClose={true}
+            viewportWidth={viewportWidth}
+          />
+        }
+      </div>
 
-
-    const openPopup = () => {
-        setNavigationPopup(true)
-    }
-
-
-    const closePopup = () => {
-        setNavigationPopup(false)
-    }
-
-
-    return (
-        <header className={`header ${location.pathname === '/' && 'header__type_color'}`}>
-
-            <div className='header__wrap'>
-                <img className='logo' src={logo} onClick={() => history.push('/')} alt='логотип' />
-                {
-                    !currentUser ?
-                        <nav className='header__nav-container'>
-                            <Link to='/signup' className='header__link_word' target='_self'>Регистрация</Link>
-                            <Link to='/signin' className='header__link_button' target='_self'>Войти</Link>
-                        </nav>
-                        :
-                        <>
-                            {viewportWidth ?
-                                <Navigation />
-                                :
-                                <img className='header__nav-icon' src={menuImage} alt='иконка меню' onClick={openPopup} />
-                            }
-                        </>
-                }
-                {
-                    navigationPopup &&
-                    <Popup
-                        closePopup={closePopup}
-                        Content={<Navigation />}
-                        buttonClose={true}
-                        viewportWidth={viewportWidth}
-                    />
-                }
-            </div>
-            
-        </header >
-    )
+    </header >
+  )
 })
 
 export default Header
